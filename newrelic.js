f4a4a1eea57bfaedbbf845b3ce353615f32078db
jalajgdev <jalaj.goyal@mail.vinove.com>
2024-09-09 09:56:19 +0530

    feat: new relic config set

--- newrelic.js
+++ newrelic.js
@@ -14,13 +14,21 @@ exports.config = {
    * Your New Relic license key.
    */
   license_key: `${process.env.NEW_RELIC}`,
+  distributed_tracing: {
+    /**
+     * Enables/disables distributed tracing.
+     *
+     * @env NEW_RELIC_DISTRIBUTED_TRACING_ENABLED
+     */
+    enabled: false
+  },
   logging: {
     /**
      * Level at which to log. 'trace' is most useful to New Relic when diagnosing
      * issues with the agent, 'info' and higher will impose the least overhead on
      * production applications.
      */
-    level: 'trace'
+    level: 'info'
   },
   /**
    * When true, all request headers except for those listed in attributes.exclude
