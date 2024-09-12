e65a7eacf90da854c8f37e1314885939bf1bca25
Raghav Maheshwari <raghav.maheshwari@mail.vinove.com>
2024-09-12 12:21:58 +0530

    update peer js port

--- client/src/environments/environment.ts
+++ client/src/environments/environment.ts
@@ -2,7 +2,7 @@
 // The build system defaults to the dev environment which uses `environment.ts`, but if you do
 // `ng build --env=prod` then `environment.prod.ts` will be used instead.
 // The list of which env maps to which file can be found in `.angular-cli.json`.
-const MY_IP = '192.168.15.247';
+const MY_IP = '192.168.2.218';
 //const MY_IP = '192.168.50.40'; // EREZ Office ip
 // const MY_IP = '192.168.50.68'; // UZI Office ip
 //const MY_IP = '192.168.0.51'; // UZI HOME
@@ -13,7 +13,7 @@ export const environment = {
   port: 8080,
   signalingServerUrl: `https://${MY_IP}:3001`,
   signalingServer: MY_IP,
-  signalingServerPort: 3000,
+  signalingServerPort: 3001,
   serverUrl: `https://${MY_IP}:8080`,
   secretKey: 'gertner-little-secret',
   recaptchaKey: '6Lc8yKspAAAAAAQ6ItzKFYZ-uJ5GG2PdWGtHZwhM',
