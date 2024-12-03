66e69528209a5a249e7b56da511d334f09f3eab2
shakilansari <shakil.ansari@mail.vinove.com>
2024-12-03 10:27:11 +0530

    fixed code

---
+++ client/src/app/common/services/skelton-video.service.ts
@@ -0,0 +1,32 @@
+import { Injectable ,ElementRef} from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SkeltonVideoService {
+
+ // private iframeUrlSubject = new BehaviorSubject<string | null>(null);
+  //iframeUrl$ = this.iframeUrlSubject.asObservable();
+
+  private gameVideoElementSubject = new BehaviorSubject<string | null>(null);
+  gameVideoElement$ = this.gameVideoElementSubject.asObservable();
+
+  setGameVideoElement(iframeaction: string | null): void {
+    console.log("=======IframeVideoaction==", iframeaction);
+    this.gameVideoElementSubject.next(iframeaction);
+  }
+
+  getGameVideoElement(): string | null {
+    return this.gameVideoElementSubject.value;
+  }
+  /*setIframeUrl(url: string | null): void {
+    console.log(" iframe url ====", url);
+    this.iframeUrlSubject.next(url);
+  }
+
+  getIframeUrl(): string | null {
+    return this.iframeUrlSubject.value;
+  }
+    */
+}
