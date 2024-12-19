1b678f243ec19d03abf810b6f15983383dd207a7
Amit Sharma <amit.sharma5@mail.vinove.com>
2024-12-19 17:16:24 +0530

    Subject:- Bug Fix for RO-34

--- server/models/patient.model.ts
+++ server/models/patient.model.ts
@@ -285,6 +285,7 @@ export const getPatientsActivities = async (therapistId, startTime, endTime) => 
 		.field(`${TABLE_NAME.PATIENT}.phone`)
 		.field(`${TABLE_NAME.USER}.user_name`)
 		.field('logged_in_at')
+		.field(`${TABLE_NAME.DEPARTMENT}.name`, 'department_name')
 		.from(TABLE_NAME.THERAPIST)
 		.left_join(
 			TABLE_NAME.THERAPIST_DEPARTMENTS,
@@ -298,11 +299,29 @@ export const getPatientsActivities = async (therapistId, startTime, endTime) => 
 		)
 		.left_join(TABLE_NAME.PATIENT, null, `${TABLE_NAME.PATIENT_DEPARTMENTS}.patient_id = ${TABLE_NAME.PATIENT}.id`)
 		.left_join(TABLE_NAME.USER, null, `${TABLE_NAME.USER}.id = ${TABLE_NAME.PATIENT}.user_id`)
+		.left_join(
+			TABLE_NAME.DEPARTMENT,
+			null,
+			`${TABLE_NAME.PATIENT_DEPARTMENTS}.department_id = ${TABLE_NAME.DEPARTMENT}.id`
+		)
 		.where(`${TABLE_NAME.THERAPIST}.id = ?`, therapistId)
 		.where(`${TABLE_NAME.USER}.active = ?`, true)
 		.toParam();
 	const patientDetails = await BaseModel.runQuery(getPatientDetails);
 	return patientDetails.rows;
+};
+
+export const getRTMList = async () => {
+	const query = squelPostgres
+		.select()
+		.field(`${TABLE_NAME.PATIENT_DEPARTMENTS}.patient_id`)
+		.from(TABLE_NAME.PATIENT_DEPARTMENTS)
+		.join(TABLE_NAME.DEPARTMENT, null, `${TABLE_NAME.PATIENT_DEPARTMENTS}.department_id = ${TABLE_NAME.DEPARTMENT}.id`)
+		.where(`LOWER(${TABLE_NAME.DEPARTMENT}.name) = ?`, 'rtm')
+		.toParam();
+
+	const result = await BaseModel.runQuery(query);
+	return result.rows;
 };
 
 export const getPatientsActivitiesData = async (patientId, startTime, endTime) => {
@@ -332,6 +351,7 @@ export const getPatientsActivitiesData = async (patientId, startTime, endTime) =
 		.left_join(TABLE_NAME.USER, null, `${TABLE_NAME.USER}.id = ${TABLE_NAME.PATIENT}.user_id`)
 		.where(`${TABLE_NAME.PATIENT}.id = ?`, patientId)
 		.where(`${TABLE_NAME.USER}.active = ?`, true)
+		.where(`LOWER(${TABLE_NAME.DEPARTMENT}.name) IN ?`, ['rtm'])
 		.toParam();
 	const patientDetails = await BaseModel.runQuery(getPatientDetails);
 	return patientDetails.rows;
