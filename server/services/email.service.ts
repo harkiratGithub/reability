0f8890d3d35222dcc16f733b96f3c9cad0be75fd
shakilansari <shakil.ansari@mail.vinove.com>
2025-01-28 14:21:16 +0530

    fixed email BCC and theRPT content

--- server/services/email.service.ts
+++ server/services/email.service.ts
@@ -2,18 +2,27 @@ import sgMail from '@sendgrid/mail';
 
 sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
-export const sendMail = async (to, subject, html, cc?, attachments?) => {
-	const emailFrom = process.env.SENGRID_FROM_EMAIL ? process.env.SENGRID_FROM_EMAIL : 'yoramfeld@gmail.com';
+export const sendMail = async (to, subject, html, cc?,attachments?: any[], bcc? ) => {	
+	let emailFrom: string;
+	let nameFrom: string;
+	if (process.env.EMAIL_LANGUAGE === 'english') {
+		emailFrom ='yoramfeld@gmail.com'; //'newp@therpt.com';
+		nameFrom = 'TheRPT';
+	} else {
+		emailFrom = process.env.SENGRID_FROM_EMAIL ? process.env.SENGRID_FROM_EMAIL : 'yoramfeld@gmail.com';
+		nameFrom = 'ReAbility';
+	}
 	const msg = {
 		to,
 		cc,
+		bcc,
 		from: {
 			email: emailFrom,
-			name: 'ReAbility',
+			name: nameFrom,
 		},
 		subject,
 		html,
-		attachments,
+		attachments: attachments || [],
 	};
 	if (process.env.NODE_ENV === 'development' || process.env.NODE_ENV === 'test') {
 		console.error(msg);
