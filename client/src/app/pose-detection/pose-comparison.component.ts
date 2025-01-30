24fe56382e6a26d51c847a8d7335403f4bdc4315
Raghav Deep Maheshwari <raghav.maheshwari@mail.vinove.com>
2025-01-30 12:38:23 +0530

    update for 1 more video

--- client/src/app/pose-detection/pose-comparison.component.ts
+++ client/src/app/pose-detection/pose-comparison.component.ts
@@ -651,15 +651,15 @@ export class PoseComparisonComponent implements OnInit, AfterViewInit {
     A: { x: number; y: number; z: number },
     B: { x: number; y: number; z: number }
   ): number {
-    const vectorAB = { x: B.x - A.x, y: B.y - A.y, z: B.z - A.z };
-    const verticalVector = { x: 0, y: 0, z: -1 };
+    const vectorAB = { x: B.x - A.x, y: B.y - A.y };
+    const verticalVector = { x: 0, y: 1 };
 
     const dotProduct =
-      vectorAB.x * verticalVector.x + vectorAB.y * verticalVector.y + vectorAB.z * verticalVector.z;
+      vectorAB.x * verticalVector.x + vectorAB.y * verticalVector.y;
 
-    const magnitudeAB = Math.sqrt(vectorAB.x ** 2 + vectorAB.y ** 2 + vectorAB.z ** 2);
+    const magnitudeAB = Math.sqrt(vectorAB.x ** 2 + vectorAB.y ** 2);
     const magnitudeVertical = Math.sqrt(
-      verticalVector.x ** 2 + verticalVector.y ** 2 + verticalVector.z ** 2
+      verticalVector.x ** 2 + verticalVector.y ** 2
     );
 
     const angleInRadians = Math.acos(
@@ -710,8 +710,8 @@ export class PoseComparisonComponent implements OnInit, AfterViewInit {
 
       if (results.poseLandmarks) {
         const tolerance = 3;
-        const leftShoulder = results.poseLandmarks[24];
-        const leftElbow = results.poseLandmarks[26];
+        const leftShoulder = results.poseLandmarks[26];
+        const leftElbow = results.poseLandmarks[28];
         const leftWrist = results.poseLandmarks[15];
         const rightShoulder = results.poseLandmarks[23];
         const rightWrist = results.poseLandmarks[25];
