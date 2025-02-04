9267ee27210f72756388b59babb213e54b365524
shakilansari <shakil.ansari@mail.vinove.com>
2025-02-04 11:42:35 +0530

    add the timezone and last login

--- server/helpers/users.helper.ts
+++ server/helpers/users.helper.ts
@@ -31,11 +31,15 @@ export const onLogIn = async (user: {
 	peerId: string;
 	email: string;
 	is_two_factor_enabled: boolean;
+	timezone:string;
 }): Promise<any> => {
-	try {		
+	try {	
+		const currentTimestamp =  new Date();	
 		await UserModel.updateById(user.id, {
 			logged_in_at: Helper.createTimeForDb(),
-			logged_out_at: Helper.createTimeForDb(),			
+			logged_out_at: Helper.createTimeForDb(),
+			user_last_login: currentTimestamp.toISOString(),
+			timezone: user.timezone,			
 		});
 		switch (user.role) {
 			case ROLE.ADMIN:
@@ -63,9 +67,10 @@ export const onLogIn = async (user: {
 					pain_level: pain_level,
 					timestamp: timestamp,
 					date_agreed_terms: date_agreed_terms,
+					timezone:timezone,
 				} = EncryptHelper.decryptJson(details[0]);
 				const RTM = details?.some((ele: { department_name: string }) => ele.department_name.toLowerCase() === 'rtm');
-				const todayEntry = await UserModel.isPatientEntryForToday(patientId);
+				const todayEntry = await UserModel.isPatientEntryForToday(patientId,timezone);
 				const isPainModelOpen = RTM && typeof todayEntry.painLevel === 'undefined' && !todayEntry.hasEntries;
 				const isMobileModelOpen =
 					RTM && new Date().getTime() - new Date(timestamp).getTime() >= 24 * 60 * 60 * 1000 ? true : false;
