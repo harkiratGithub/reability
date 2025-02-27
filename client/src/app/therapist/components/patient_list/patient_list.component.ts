ab25bc49145ff3aa046e0b44daf540e64893c670
shakilansari <shakil.ansari@mail.vinove.com>
2025-02-27 16:33:59 +0530

    fixed

--- client/src/app/therapist/components/patient_list/patient_list.component.ts
+++ client/src/app/therapist/components/patient_list/patient_list.component.ts
@@ -444,7 +444,6 @@ export class PatientListComponent implements OnInit, OnDestroy {
             this.appActions.setMessageRTMModal(successMessage);
           })
           .catch((err) => {
-            console.log("====errshakil==",err);
             this.appActions.setMessageRTMModal(err || 'Error occurred. Please try again later');
             console.error('Error:', err);
           });
