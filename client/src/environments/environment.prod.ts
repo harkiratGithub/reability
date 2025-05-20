874d76ab2e26dbf13f7f7d16f4f756e1335dd335
shakilansari <shakil.ansari@mail.vinove.com>
2025-05-20 11:51:15 +0530

    us-therpt-node 20 build

--- client/src/environments/environment.prod.ts
+++ client/src/environments/environment.prod.ts
@@ -1,10 +1,10 @@
 
 export const environment = {
-  production: true,
-  secretKey: 'gertner-little-secret',
-  signalingServerPort:443,
-  signalingServer:'signaling-us.reabilityonline.com',
-  signalingServerUrl: 'https://signaling-us.reabilityonline.com',
-  recaptchaKey: '6Lc8yKspAAAAAAQ6ItzKFYZ-uJ5GG2PdWGtHZwhM',
-  serverUrl: ''
-};
+    production: true,
+    secretKey: 'gertner-little-secret',
+    signalingServerPort:443,
+    signalingServer:'signaling-us.therpt.com',
+    signalingServerUrl: 'https://signaling-us.therpt.com',
+    recaptchaKey: '6Lc8yKspAAAAAAQ6ItzKFYZ-uJ5GG2PdWGtHZwhM',
+    serverUrl: ''
+  };
