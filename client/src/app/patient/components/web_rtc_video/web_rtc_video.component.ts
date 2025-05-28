b70fbf1070997caf10a98c26f3fd83f1fbf1fca2
Raghav Deep Maheshwari <raghav.maheshwari@mail.vinove.com>
2025-05-28 17:49:12 +0530

    update fro STVT

--- client/src/app/patient/components/web_rtc_video/web_rtc_video.component.ts
+++ client/src/app/patient/components/web_rtc_video/web_rtc_video.component.ts
@@ -2408,8 +2408,8 @@ export class WebRTCVideoComponent implements OnInit, AfterViewInit, OnDestroy, O
           // Additional code to draw pose landmarks and connections on the canvas
           results.poseLandmarks.forEach((landmark, index) => {
             // if (this.videoIndex == 3) {
-            // if (this.landmarksPointer.includes(index)) {
-            if (index == 11 || index == 12) {
+            if (this.landmarksPointer.includes(index)) {
+              // if (index == 11 || index == 12) {
               canvasCtx.beginPath();
               canvasCtx.arc(
                 landmark.x * canvasElement.width,
