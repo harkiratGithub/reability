de4297331478e9e559befac910be5aa22eb6e87d
Raghav Deep Maheshwari <raghav.maheshwari@mail.vinove.com>
2025-06-12 16:10:57 +0530

    update package-lock.json and package.json

--- client/src/environments/environment.prod.ts
+++ client/src/environments/environment.prod.ts
@@ -3,8 +3,8 @@ export const environment = {
   production: true,
   secretKey: 'gertner-little-secret',
   signalingServerPort: 443,
-  signalingServer: 'signaling-us.reabilityonline.com',
-  signalingServerUrl: 'https://signaling-us.reabilityonline.com',
+  signalingServer: 'signaling-us.therpt.com',
+  signalingServerUrl: 'https://signaling-us.therpt.com',
   recaptchaKey: '6Lc8yKspAAAAAAQ6ItzKFYZ-uJ5GG2PdWGtHZwhM',
   serverUrl: ''
 };
