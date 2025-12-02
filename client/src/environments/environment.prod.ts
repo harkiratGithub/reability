7870c34e438d1e0f4af3f4e6f095cb1fc74515b5
shakilansari <shakil.ansari@mail.vinove.com>
2025-12-02 18:15:19 +0530

    fixed uk-new -env

--- client/src/environments/environment.prod.ts
+++ client/src/environments/environment.prod.ts
@@ -1,10 +1,10 @@
 
 export const environment = {
-  production: true,
-  secretKey: 'gertner-little-secret',
-  signalingServerPort: 443,
-  signalingServer: 'signaling-us.therpt.com',
-  signalingServerUrl: 'https://signaling-us.therpt.com',
-  recaptchaKey: '6Lc8yKspAAAAAAQ6ItzKFYZ-uJ5GG2PdWGtHZwhM',
-  serverUrl: ''
-};
+    production: true,
+    secretKey: 'gertner-little-secret',
+    signalingServerPort: 443,
+    signalingServer: 'signaling-uk.reabilityonline.com',
+    signalingServerUrl: 'https://signaling-uk.reabilityonline.com',
+    recaptchaKey: '6Lc8yKspAAAAAAQ6ItzKFYZ-uJ5GG2PdWGtHZwhM',
+    serverUrl: ''
+  };
