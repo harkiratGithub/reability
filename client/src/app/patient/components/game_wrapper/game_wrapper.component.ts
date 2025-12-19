951983df403b6d759faf00b83fb9124a296f1482
shakilansari <shakil.ansari@mail.vinove.com>
2025-12-19 10:59:22 +0530

    enable crousel text in uk

--- client/src/app/patient/components/game_wrapper/game_wrapper.component.ts
+++ client/src/app/patient/components/game_wrapper/game_wrapper.component.ts
@@ -1021,7 +1021,7 @@ export class GameWrapperComponent implements OnInit, OnDestroy {
   }*/
 
   startCarousel(): void {
-    /*if (this.isPopupVisible) {
+    if (this.isPopupVisible) {
       const baseSpeed = 200; // Base speed (ms) per character
       const minInterval = 3000; // Minimum interval for very short text
       const maxInterval = 20000; // Maximum interval for very long text
@@ -1042,7 +1042,6 @@ export class GameWrapperComponent implements OnInit, OnDestroy {
       // Start the first cycle
       updateText();
     }
-      */
   }
 
 }
