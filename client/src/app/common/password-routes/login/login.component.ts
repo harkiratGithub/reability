b5e413a0c7b07a90b72a4fb94aad9f33991147e6
shakilansari <shakil.ansari@mail.vinove.com>
2026-03-06 10:42:15 +0530

    fixed whitespace login

--- client/src/app/common/password-routes/login/login.component.ts
+++ client/src/app/common/password-routes/login/login.component.ts
@@ -90,7 +90,18 @@ export class LoginPageComponent implements OnInit, OnDestroy {
         this.f.username.setErrors(null);
         const userTimezone = Intl.DateTimeFormat().resolvedOptions().timeZone;
         const token = await this.recaptchaV3Service.execute('login').toPromise();
-        const user = await this.ajax.login(this.f.username.value, this.f.password.value, token,userTimezone).toPromise();
+        // Trim username & password
+        const username = (this.f.username.value || '').trim();
+          const password = (this.f.password.value || '').trim();
+          this.f.username.setValue(username);
+          this.f.password.setValue(password);
+          const user = await this.ajax.login(
+            username,
+            password,
+            token,
+            userTimezone
+          ).toPromise();
+        //const user = await this.ajax.login(this.f.username.value, this.f.password.value, token,userTimezone).toPromise();
         if (window && (window as any).NREUM) {
           (window as any).NREUM.addPageAction('LoginSuccess', {
             username: this.f.username.value,
